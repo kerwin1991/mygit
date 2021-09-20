9b4d32f81c5991083603c8ee2f1ac82582c5a9ed
kk <1318699951@qq.com>
2021-09-20 10:55:15 +0800

    链表回文

--- msb/wx-zcy/zcy-basic/src/main/java/com/wx/class03/IsPalindromeList.java
+++ msb/wx-zcy/zcy-basic/src/main/java/com/wx/class03/IsPalindromeList.java
@@ -52,6 +52,7 @@ public class IsPalindromeList {
             n2 = n2.next.next;
         }
         // n1 在第一个3(偶数)或者5(基数)位置了。。n2在倒数第二个位置或者在最后一个
+
         // 2 后半段翻转方向，n1指向null 后面翻转
         // n2 变成另一个指针 和n1 完成后半段翻转
         n2 = n1.next;
@@ -68,21 +69,36 @@ public class IsPalindromeList {
         n3 = n1; // 保存了尾巴
         n2 = head;
 
-        // 2 判断是否回文，n1 n2 一头一尾
+        // 3 判断是否回文，n1 n2 一头一尾
+        boolean ret = true;
+        while (n1 != null && n2 != null) {
+            if (n1.value != n2.value) {
+                ret = false;
+                break;
+            }
+            n1 = n1.next;
+            n2 = n2.next;
+        }
 
+        // 4 把后半段的指向恢复 相当于链表的反转
+        /*
+        1->2->3->null  <-3<-2<-1
+        1->2->3->      3->2->1->null
 
+        1->2->3->5->null  <-3<-2<-1
+        1->2->3->5->      3->2->1->null
 
-
-
-
-
-
-
-
-
-
-
-        return true;
+        此时n1相当于头结点了哈
+         */
+        n2 = n3.next;
+        n3.next = null;
+        while (n2 != null) {
+            n1 = n2.next;
+            n2.next = n3;
+            n3 = n2;
+            n2 = n1;
+        }
+        return ret;
     }
 
 
